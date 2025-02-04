eca84acb6dd4d17a776bad2cde5fef1f0c847534
Ashim-Stha <ashimshrestha2384@gmail.com>
2025-02-04 15:31:41 +0545

    songRepository

--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -1,9 +1,12 @@
 import { Injectable, Scope } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Song } from './song.entity';
 
 @Injectable({
-  scope: Scope.TRANSIENT, //create a new dedicated instance of SongsController on every incoming req - doesnot cache
+  // scope: Scope.TRANSIENT, //create a new dedicated instance of SongsController on every incoming req - doesnot cache
 })
 export class SongsService {
+  constructor(private songsRepository: Repository<Song>) {}
   private readonly songs = [];
 
   create(song) {
