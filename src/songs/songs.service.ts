274343736422f997677e6193fd78160f8a4adf7a
Ashim-Stha <ashimshrestha2384@gmail.com>
2025-02-04 15:40:34 +0545

    create songs

--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -1,21 +1,26 @@
 import { Injectable, Scope } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Song } from './song.entity';
+import { CreateSongDTO } from './dto/create-song-dto';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable({
   // scope: Scope.TRANSIENT, //create a new dedicated instance of SongsController on every incoming req - doesnot cache
 })
 export class SongsService {
-  constructor(private songsRepository: Repository<Song>) {}
-  private readonly songs = [];
+  constructor(
+    @InjectRepository(Song) private songsRepository: Repository<Song>,
+  ) {}
+  async create(songDTO: CreateSongDTO): Promise<Song> {
+    const song = new Song();
+    song.title = songDTO.title;
+    song.artists = songDTO.artists;
+    song.duration = songDTO.duration;
+    song.lyrics = songDTO.lyrics;
+    song.releasedDate = songDTO.releasedDate;
 
-  create(song) {
-    this.songs.push(song);
-    return this.songs;
+    return await this.songsRepository.save(song);
   }
 
-  findAll() {
-    // throw new Error('Error in DB while fetching record');
-    return this.songs;
-  }
+  findAll() {}
 }
