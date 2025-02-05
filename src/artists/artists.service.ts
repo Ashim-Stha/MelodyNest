6178dca927acceef11ac435d935d0d99f1c23b5c
Ashim-Stha <ashimshrestha2384@gmail.com>
2025-02-05 09:15:23 +0545

    findArtist

--- src/artists/artists.service.ts
+++ src/artists/artists.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Artist } from './artist.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ArtistsService {}
+export class ArtistsService {
+  constructor(
+    @InjectRepository(Artist) private artistRepository: Repository<Artist>,
+  ) {}
+
+  findArtist(userId: number): Promise<Artist> {
+    return this.artistRepository.findOneBy({ user: { id: userId } });
+  }
+}
