727e03d0f4f70d1aa44cd43e125b875411a6a9d5
Ashim-Stha <ashimshrestha2384@gmail.com>
2025-02-06 09:34:19 +0545

    modified data-source.ts for config

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,17 +1,54 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {
+  TypeOrmModuleAsyncOptions,
+  TypeOrmModuleOptions,
+} from '@nestjs/typeorm';
 import { DataSource, DataSourceOptions } from 'typeorm';
+
+// export const dataSourceOptions: DataSourceOptions = {
+//   type: 'postgres',
+//   host: 'localhost',
+//   port: 5432,
+//   username: 'postgres',
+//   password: 'root',
+//   database: 'Spotify',
+//   entities: ['dist/**/*.entity.js'], //1
+//   synchronize: false, // 2
+//   migrations: ['dist/db/migrations/*.js'], // 3
+// };
+
+export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: async (
+    configService: ConfigService,
+  ): Promise<TypeOrmModuleOptions> => {
+    return {
+      type: 'postgres',
+      host: configService.get<string>('dbHost'),
+      port: configService.get<number>('dbPort'),
+      username: configService.get<string>('username'),
+      database: configService.get<string>('dbName'),
+      password: configService.get<string>('password'),
+      entities: ['dist/**/*.entity.js'],
+      synchronize: false,
+      migrations: ['dist/db/migrations/*.js'],
+    };
+  },
+};
+//
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: '#Awseem23845',
-  database: 'Spotify',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.USERNAME,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
   entities: ['dist/**/*.entity.js'], //1
   synchronize: false, // 2
   migrations: ['dist/db/migrations/*.js'], // 3
 };
 
-//
-const dataSource = new DataSource(dataSourceOptions);
+const dataSource = new DataSource(dataSourceOptions); //4
 export default dataSource;
